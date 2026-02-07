585628fd84fb95f853fdc1d577ce0ecd24e4d17a
PrathamNcrypt <prathamnebhinani16@gmail.com>
2026-02-08 01:14:43 +0530

    feat: implement URL parsing and query parameter handling

--- server/index.js
+++ server/index.js
@@ -1,24 +1,28 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
-const myServer = http.createServer((req, res)=>{
-    console.log(req.headers);
-    const log = `${Date.now()}: ${req.url} New Req Received\n`;
-    fs.appendFile("log.txt", log, (error,data)=>{
-        switch(req.url){
-            case  "/":
-            res.end("Homepage");
-            break;
-             case  "/about":
-            res.end("My name is Pratham");
-            break;
-             default:
-            res.end("404 Not Found");
 
-        }
-        
-    })
-
-})
+const myServer = http.createServer((req, res) => {
+  console.log(req.headers);
+  const log = `${Date.now()}: ${req.url} New Req Received\n`;
+  const myUrl = url.parse(req.url, true);
+  fs.appendFile("log.txt", log, (error, data) => {
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("Homepage");
+        break;
+      case "/about":
+        const username = myUrl.query.myname;
+        res.end(`Hi, ${username}`);
+        break;
+      case "/search":
+        const search = myUrl.query.search_query;
+        res.end(`Here are your search results for ${search}`);
+      default:
+        res.end("404 Not Found");
+    }
+  });
+});
 
 myServer.listen(8000, ()=> console.log("Server Started"));
